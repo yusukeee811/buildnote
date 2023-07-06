1d1428895757189aefa464035201479760611b14
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-07-06 07:27:42 +0000

    [Update]ヘッダー,[Add]アクティブストレージ、アソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
+  belongs_to :user
+  has_many :post_likes,     dependent: :destroy
+  has_many :post_comments,  dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+
+  has_many :posts,         dependent: :destroy
+  has_many :post_likes,    dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :trainings,     dependent: :destroy
+  # フォローした時の関係
+  has_many :relationships,            class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされた時の関係
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォロー、フォロワー一覧画面で使用
+  has_many :followings, through: :relationships,            source: :followed
+  has_many :followers,  through: :reverse_of_relationships, source: :follower
+
+  # フォロー時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォロー解除時の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end

1687829318fc57eb42c6ffb9737630571f2da7cf
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-07-11 16:20:27 +0000

    [Add]いいね機能非同期通信化

--- app/controllers/public/post_likes_controller.rb
+++ app/controllers/public/post_likes_controller.rb
@@ -1,16 +1,13 @@
 class Public::PostLikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @post_like = current_user.post_likes.new(post_id: @post.id)
-    @post_like.post_id = @post.id
-    @post_like.save
-    redirect_to posts_path
+    post_like = current_user.post_likes.new(post_id: @post.id)
+    post_like.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @post_like = @post.post_likes.find_by(post_id: @post.id)
-    @post_like.destroy
-    redirect_to posts_path
+    post_like = current_user.post_likes.find_by(post_id: @post.id)
+    post_like.destroy
   end
 end

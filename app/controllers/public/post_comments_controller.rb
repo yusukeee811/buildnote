a461263c56786cbb8ab32a409639dc3b45980795
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-07-11 16:44:28 +0000

    [Add]コメント機能非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,14 +4,12 @@ class Public::PostCommentsController < ApplicationController
     @post_comment = current_user.post_comments.new(post_comment_params)
     @post_comment.post_id = @post.id
     @post_comment.save
-    redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @post_comment = @post.post_comments.find(params[:id])
-    @post_comment.destroy
-    redirect_to posts_path
+    post_comment = @post.post_comments.find(params[:id])
+    post_comment.destroy
   end
 
   private
